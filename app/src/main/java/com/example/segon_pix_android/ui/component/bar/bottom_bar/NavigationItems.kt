8ad68cfb8879c7df892a32dc7976e44ea1d362d4
seg <segnities007@gmail.com>
2025-02-21 17:50:11 +0900

    Implement: Implement Topbar and BottomNavigationBar.

---
+++ app/src/main/java/com/example/segon_pix_android/ui/component/bar/bottom_bar/NavigationItems.kt
@@ -0,0 +1,7 @@
+package com.example.segon_pix_android.ui.component.bar.bottom_bar
+
+interface NavigationItems {
+    val selectedIcons: List<Int>
+    val unSelectedIcons: List<Int>
+    val labels: List<String>
+}
